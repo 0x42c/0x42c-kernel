6a94b8a5c1abaf34a7e3042a9c68899950d2665c
Drew DeVault <sir@cmpwn.com>
2012-10-05 14:56:55 -0600

    Updated organic and added drivers

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir ..\bin
-organic.exe ../src/base.dasm ../bin/kernel.bin --include ../inc/ --listing ../bin/kernel.lst --working-directory ../src/
+organic.exe ../src/base.dasm ../bin/kernel.bin --include ../inc/ --listing ../bin/kernel.lst --working-directory ../src/ --debug-mode
 @echo on
