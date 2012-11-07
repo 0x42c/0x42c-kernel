efe2b97d79a8834563e3cfad7d1e74c2e85d7ac3
Drew DeVault <sir@cmpwn.com>
2012-11-07 01:00:20 -0700

    Updated build scripts and added debug config

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,4 @@
 @echo off
+msbuild.exe ../tools/disk-img/diskimg.sln
 organic.exe ../src/base.dasm ../bin/kernel.bin --include ../inc/ --listing ../bin/kernel.lst --working-directory ../src/ --json ../bin/kernel.json
 @echo on
--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+xbuild ../tools/disk-img/diskimg.sln
 mono organic.exe ../src/base.dasm ../bin/kernel.bin --include ../inc/ --listing ../bin/kernel.lst --working-directory ../src/ --json ../bin/kernel.json
